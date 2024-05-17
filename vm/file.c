4d5c0f46acd3fa40340f945ab1ca80f1589056ff
Arklimits <Arklimits@users.noreply.github.com>
2024-05-17 12:16:41 +0000

    mmap 구현

--- vm/file.c
+++ vm/file.c
@@ -2,6 +2,10 @@
 
 #include "vm/vm.h"
 
+/** Project 3: Memory Mapped Files */
+#include "threads/vaddr.h"
+#include "userprog/process.h"
+
 static bool file_backed_swap_in(struct page *page, void *kva);
 static bool file_backed_swap_out(struct page *page);
 static void file_backed_destroy(struct page *page);
@@ -41,10 +45,34 @@ static void file_backed_destroy(struct page *page) {
     struct file_page *file_page UNUSED = &page->file;
 }
 
-/* Do the mmap */
+/** Project 3: Memory Mapped Files - Memory Mapping - Do the mmap */
 void *do_mmap(void *addr, size_t length, int writable, struct file *file, off_t offset) {
+    struct file *mfile = file_reopen(file);
+    void *ori_addr = addr;
+    size_t read_bytes = (length > file_length(file)) ? file_length(file) : length;
+    size_t zero_bytes = PGSIZE - read_bytes % PGSIZE;
+
+    while (read_bytes > 0 || zero_bytes > 0) {
+        size_t page_read_bytes = read_bytes < PGSIZE ? read_bytes : PGSIZE;
+        size_t page_zero_bytes = PGSIZE - page_read_bytes;
+
+        struct container *container = (struct container *)malloc(sizeof(struct container));
+        container->file = file;
+        container->offset = offset;
+        container->page_read_bytes = page_read_bytes;
+
+        if (!vm_alloc_page_with_initializer(VM_ANON, addr, writable, lazy_load_segment, container))
+            return false;
+
+        read_bytes -= page_read_bytes;
+        zero_bytes -= page_zero_bytes;
+        addr += PGSIZE;
+        offset += page_read_bytes;
+    }
+    
+    return ori_addr;
 }
 
-/* Do the munmap */
+/** Project 3: Memory Mapped Files - Memory Mapping - Do the munmap */
 void do_munmap(void *addr) {
 }
