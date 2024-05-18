6510196fae95fda9e4be19296559df034bcd49b5
Arklimits <Arklimits@users.noreply.github.com>
2024-05-18 01:19:33 +0000

    사소한거 수정
    Co-authored-by: Cheongwoo Nam <skacjddn7@gmail.com>
    Co-authored-by: Heungkuk_201702305 <hkyoo988@users.noreply.github.com>

--- vm/file.c
+++ vm/file.c
@@ -63,7 +63,7 @@ static void file_backed_destroy(struct page *page) {
 void *do_mmap(void *addr, size_t length, int writable, struct file *file, off_t offset) {
     struct file *mfile = file_reopen(file);
     void *ori_addr = addr;
-    size_t read_bytes = (length > file_length(file)) ? file_length(file) : length;
+    size_t read_bytes = (length > file_length(mfile)) ? file_length(mfile) : length;
     size_t zero_bytes = PGSIZE - read_bytes % PGSIZE;
 
     ASSERT((read_bytes + zero_bytes) % PGSIZE == 0);
